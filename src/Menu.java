c1bea5a31280b9d2cad730ab045988ae116cb07d
Your Name <you@example.com>
2024-05-09 22:30:23 -0300

    batalha e etc

--- src/Menu.java
+++ src/Menu.java
@@ -41,8 +41,9 @@ public class Menu {
 		
 		System.out.println();
 		System.out.println("1 - Listar meus pokemons");
-		System.out.println("2 - Adicionar um pokemon");
-		System.out.println("3 - Remover um pokemon");
+		System.out.println("2 - Batalhar");
+		System.out.println("3 - Adicionar um pokemon");
+		System.out.println("4 - Remover um pokemon");
 		System.out.println();
 
 		System.out.print("Selecione uma opção: ");
@@ -53,9 +54,12 @@ public class Menu {
 				listarMeusPokemons();
 				break;
 			case 2:
-				adicionarPokemon();
+				batalhar();
 				break;
 			case 3:
+				adicionarPokemon();
+				break;
+			case 4:
 				removerPokemon();
 				break;
 		}
@@ -78,14 +82,12 @@ public class Menu {
 		}else {
 			interagirComPokemon(pokemonOpt.get());
 		}
-		
 	}
 	
 	public void interagirComPokemon(Pokemon pokemon) {
 		System.out.println("POKEMON SELECIONADO: " + pokemon.getNome());
 		
 		System.out.println(pokemon);
-		
 	}
 	
 	private void adicionarPokemon(){
@@ -140,6 +142,98 @@ public class Menu {
 			System.out.println("Pokemon com o id: " + opcao + " removido com sucesso.");
 			menuPrincipal();
 		}
+	}
+	
+	private void batalhar() {
+		System.out.println("SUA POKEDEX");
+		
+		meusPokemons.forEach(System.out::println);
+		System.out.print("Escolha o pokemon que vai à batalha: ");
+		opcao = sc.nextInt();
+		
+		Pokemon pokemonAliado = meusPokemons.stream().filter(pokemon -> pokemon.id == opcao).findFirst().get();
+		
+		System.out.println();
+		
+		System.out.println("POKEDEX DO SISTEMA");
+		todosPokemons.forEach(System.out::println);
+		System.out.print("Escolha o pokemon que quer capturar: ");
+		opcao = sc.nextInt();
+		
+		Pokemon pokemonInimigo = todosPokemons.stream().filter(pokemon -> pokemon.id == opcao).findFirst().get();
+		
+		campoDeBatalha(pokemonAliado, pokemonInimigo);
+	}
+	
+	private void campoDeBatalha(Pokemon pokemonAliado, Pokemon pokemonInimigo) {
+		System.out.println(" _____                                                                                                 _____ \r\n"
+				+ "( ___ )                                                                                               ( ___ )\r\n"
+				+ " |   |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|   | \r\n"
+				+ " |   |  ___ _   _ ___ ____ ___    _    _   _ ____   ___    ____    _  _____  _    _     _   _    _     |   | \r\n"
+				+ " |   | |_ _| \\ | |_ _/ ___|_ _|  / \\  | \\ | |  _ \\ / _ \\  | __ )  / \\|_   _|/ \\  | |   | | | |  / \\    |   | \r\n"
+				+ " |   |  | ||  \\| || | |    | |  / _ \\ |  \\| | | | | | | | |  _ \\ / _ \\ | | / _ \\ | |   | |_| | / _ \\   |   | \r\n"
+				+ " |   |  | || |\\  || | |___ | | / ___ \\| |\\  | |_| | |_| | | |_) / ___ \\| |/ ___ \\| |___|  _  |/ ___ \\  |   | \r\n"
+				+ " |   | |___|_| \\_|___\\____|___/_/   \\_\\_| \\_|____/ \\___/  |____/_/   \\_\\_/_/   \\_\\_____|_| |_/_/   \\_\\ |   | \r\n"
+				+ " |___|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|___| \r\n"
+				+ "(_____)                                                                                               (_____)");
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println(pokemonAliado);
+		System.out.println();
+		System.out.println(pokemonInimigo);
+		
+			
+		FotoPokemon.mostrar(pokemonAliado.nome);
+		System.out.print("\r\n"
+				+ "\\ \\/ /\r\n"
+				+ " \\  / \r\n"
+				+ " /  \\ \r\n"
+				+ "/_/\\_\\");
+		FotoPokemon.mostrar(pokemonInimigo.nome);
+		
+		try {
+			Thread.sleep(1500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		if(pokemonInimigo.fraquezas.contains(pokemonAliado.tipagem)) {
+			System.out.println(pokemonAliado.getNome() + " ganhou a batalha pois a tipagem é melhor!");
+			try {
+				Thread.sleep(1000);
+				
+				System.out.println("  ____    _    ____ _____ _   _ ____      _    ____   ___  \r\n"
+						+ " / ___|  / \\  |  _ \\_   _| | | |  _ \\    / \\  |  _ \\ / _ \\ \r\n"
+						+ "| |     / _ \\ | |_) || | | | | | |_) |  / _ \\ | | | | | | |\r\n"
+						+ "| |___ / ___ \\|  __/ | | | |_| |  _ <  / ___ \\| |_| | |_| |\r\n"
+						+ " \\____/_/   \\_\\_|    |_|  \\___/|_| \\_\\/_/   \\_\\____/ \\___/ ");
+				
+				System.out.println("PARABÉNS, você ganhou e capturou o pokemon: " + pokemonInimigo.nome);
+				meusPokemons.add(pokemonInimigo);
+				todosPokemons.remove(pokemonInimigo);
+				
+				Thread.sleep(1000);
+				menuPrincipal();
+				
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+		}else {
+			System.out.println(pokemonAliado.getNome() + " perdeu a batalha pois a tipagem é pior!");
+			try {
+				Thread.sleep(1000);
+				menuPrincipal();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
+		
 	}
 	
 }
